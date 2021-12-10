f51db49cc0042010118ad12d1bdaf9ec849ce504
Sarah Nosal <nosalsm@gmail.com>
2021-12-10 15:53:07 -0600

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,21 @@
 import React from "react";
+import NavBar from './NavBar';
+import Home from './Home';
 
-function NavBar() {
+function About() {
   return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
+  <div id='about'>
+    <h2>About</h2>
     </div>
-  );
+  )
 }
-
-{/* write an <About> component here */}
-
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
